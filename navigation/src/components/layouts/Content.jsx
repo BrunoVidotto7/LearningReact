1d67554b831a1915d77310a7638bff3ee0129d43
Bruno Mata <bruno.damata@qikserve.com>
2023-08-24 10:48:49 -0300

    Starting Navigation project

---
+++ navigation/src/components/layouts/Content.jsx
@@ -0,0 +1,10 @@
+import './Content.css'
+import React from "react";
+
+const Content = props => (
+    <main className="Content">
+        Content Component
+    </main>
+)
+
+export default Content
---
+++ navigation/src/views/examples/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Home = props => (
+    <div className="Home">
+        <h1>Beginning</h1>
+        <p>Welcome!</p>
+    </div>
+)
+
+export default Home
